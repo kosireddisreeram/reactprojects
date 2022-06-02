b07470aa105114c5f02dcdeae02406951929f6e8
kosireddisreeram <78956214+kosireddisreeram@users.noreply.github.com>
2022-06-02 11:22:27 +0530

    weathercompleted

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { useState } from 'react';
 function App() {
+  const [city,setCity] = useState("")
+  const [result,setResult]= useState("");
+  const inputHandler = (e) =>{
+setCity(e.target.value)
+  }
+  const formHandler=(e)=>{
+    e.preventDefault();
+
+    // getting the api using the fetch
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`)
+    .then((response)=>response.json())
+      .then((data)=>{
+        const kelvin = data.main.temp;
+        const celsius = Math.round(kelvin-273.15);
+        console.log(celsius)
+         setResult(`the temperatue ${city} is ${celsius}`);
+
+        setCity('');
+      })
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Weather App</h1>
+           <form onSubmit={formHandler}>
+             <input type="text"  value={city} onChange={inputHandler}/> <br></br>
+             <input type="submit" name="search"/>
+           </form>
+           <h1>{result}</h1>
+   </div>
   );
 }
 
